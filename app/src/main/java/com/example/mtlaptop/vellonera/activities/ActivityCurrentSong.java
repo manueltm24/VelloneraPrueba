222b12bc5657cc406dc7332b408b554c7ac75645
manueltm24 <manutm24@gmail.com>
2015-12-13 17:10:41 -0400

    Subida de Proyecto a Github

---
+++ app/src/main/java/com/example/mtlaptop/vellonera/activities/ActivityCurrentSong.java
@@ -0,0 +1,22 @@
+package com.example.mtlaptop.vellonera.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import  com.example.mtlaptop.vellonera.R;
+
+/**
+ * Created by AcadGildMentor on 6/9/2015.
+ */
+public class ActivityCurrentSong extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.act_current_song);
+
+
+    }
+}
